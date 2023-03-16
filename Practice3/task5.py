205777ead24a2d16aefa80a4da30d99ada360edc
kristianaindrike <kristianaindrike@gmail.com>
2023-03-16 18:03:00 +0200

    5th task corrected

--- Practice3/task5.py
+++ Practice3/task5.py
@@ -1,11 +1,15 @@
 # Write a program that takes an integer as input and prints out whether that integer is prime or not.
 
 number = int(input("Enter your number: "))
-if number == 1:
-    print(number, "is not a prime number.")
-elif number > 1:
+prime = True
+if number <= 1:
+    print(number, "is neither prime nor not prime number.")
+else:
     for i in range (2,number):
             if (number % i) == 0:
-                  print(number,"is not a prime number.")
+                  prime = False
+                  break
+if prime == True:
+     print(number,"is a prime number.")
 else:
-     print(number + "is prime number.")
+     print(number, "is not a prime number.")
